b3f756badbefd1631bfd2493950358f716d2113d
Your Name <you@example.com>
2024-05-19 01:07:32 -0300

    autenticação restApi

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from  flask import Flask, request
 from flask_restful import Resource, Api
 from models import Pessoas, Atividades
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+USUARIOS = {
+    'rafael':'123',
+    'galeani';'432'
+}
+@auth.verify_password()
+def verificacao(login, senha):
+    print('validando usuario')
+    print(USUARIOS.get(login) == senha)
+    if not(login, senha):
+        return False
+    return USUARIOS.get(login) == senha
+
 class Pessoa(Resource):
+    @auth.login_required
     def get(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -44,6 +59,7 @@ class Pessoa(Resource):
         return{'status':'sucesso', 'mensagem':mensagem}
 
 class ListaPessoas(Resource):
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all()
         response = [{'id':i.id, 'nome':i.nome, 'idade':i.idade} for i in pessoas]
